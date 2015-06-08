6facbf0dec64553a24196aab8b3d8ad84418f16c
Nguyen Nguyen <ntn@NTN.local>
2015-06-08 14:19:43 +0700

    WithNewMessage error hander

--- helpers/apiErrors/error.go
+++ helpers/apiErrors/error.go
@@ -9,3 +9,24 @@ type Error struct {
 func (e *Error) Error() string {
 	return e.Message
 }
+
+func (e *Error) WithNewMessage(message string) *Error {
+	return &Error{
+		Id:      e.Id,
+		Message: message,
+		Status:  e.Status,
+	}
+}
+
+var (
+	SERVER_ERROR = Error{
+		Id:      "SERVER_ERROR",
+		Message: "The server encountered an unexpected condition that prevented it from fulfilling the request.",
+		Status:  500,
+	}
+	SERVER_TEMPORARILY_UNAVAILBLE = Error{
+		Id:      "SERVER_TEMPORARILY_UNAVAILBLE",
+		Message: "The server is temporarily unavailable.",
+		Status:  500,
+	}
+)
